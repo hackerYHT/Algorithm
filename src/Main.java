46d49e3b17a094595f10794880f8445be7011771
hackerYHT <2418170957@qq.com>
2022-04-16 22:28:54 +0800

    sort

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package src;
+
+public class Main{
+    public static void main(String[] args) {
+        int q = 0;
+        int[] res = new int[]{0, 1, 2, 4, 5, 3, 6, 9, 2, 4, 7, 8};
+        Sort.insertSort(res);
+        System.out.println(q);
+    }
+}
