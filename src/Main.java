bc3e87dd1f23c1bfcb6e170a45537e4546edf7e9
hackerYHT <2418170957@qq.com>
2022-04-17 20:56:08 +0800

    mergeSort

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main{
     public static void main(String[] args) {
         int q = 0;
         int[] res = new int[]{0, 1, 2, 4, 5, 3, 6, 9, 2, 4, 7, 8};
-        Sort.insertSort(res);
+        Sort.mergeSort(res);
         System.out.println(q);
     }
 }
--- src/Sort.java
+++ src/Sort.java
@@ -1,6 +1,7 @@
 package src;
 
 public class Sort {
+    //插入排序
     public static void insertSort(int[] arr){
         for(int i = 1; i < arr.length; i++){
             int key = arr[i];
@@ -9,5 +10,29 @@ public class Sort {
                 arr[j+1] = arr[j--];
             arr[j+1] = key;
         }
+    }
+
+    //归并排序
+    public static void mergeSort(int[] arr){
+        Sort.ms(arr, 0, arr.length-1);
+    }
+    public static void ms(int[] arr, int p, int r){
+        if(r > p) {
+            int q = (p + r) / 2;
+            ms(arr, p, q);
+            ms(arr, q+1, r);
+            merge(arr, p, q, r);
+        }
+    }
+    private static void merge(int[] arr, int p, int q, int r){
+        int[] tmp = new int[r - p + 1];
+        int left = p, right = q + 1, i = 0;
+        while(left <= q && right <= r){
+            tmp[i++] = arr[left] < arr[right] ? arr[left++] : arr[right++];
+        }
+        while(left <= q) tmp[i++] = arr[left++];
+        while(right <= r) tmp[i++] = arr[right++];
+        i = 0;
+        while(i < tmp.length) arr[p+i] = tmp[i++];
     }
 }
