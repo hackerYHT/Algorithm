6acf1612379c5ad580757778f955ee74d4bba1ce
hackerYHT <2418170957@qq.com>
2022-04-25 22:37:46 +0800

    heapSort

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: yht
+ * @Date: 2022-04-21 04:13:12
+ * @LastEditors: yht
+ * @LastEditTime: 2022-04-25 22:32:53
+ */
 package src;
 
 public class Main{
     public static void main(String[] args) {
         int[] res = new int[]{0, 1, 2, 4, 5, 3, 6, 9, 2, 4, 7, 8};
-        Sort.mergeSort(res);
+        Sort.heapSort(res);
         System.out.println(5);
     }
     public static int getUnsignedByte (byte data){
--- src/Sort.java
+++ src/Sort.java
@@ -1,13 +1,13 @@
+package src;
+
 /*
  * @Descripttion: 排序算法 
  * @version: 
  * @Author: yht
  * @Date: 2022-04-17 20:57:57
  * @LastEditors: yht
- * @LastEditTime: 2022-04-21 05:15:56
+ * @LastEditTime: 2022-04-25 22:35:24
  */
-package src;
-
 public class Sort {
     /**
      * @name: 插入排序 
@@ -53,5 +53,38 @@ public class Sort {
         while(right <= r) tmp[i++] = arr[right++];
         i = 0;
         while(i < tmp.length) arr[p+i] = tmp[i++];
+    }
+    /**
+     * @name:堆排序 
+     * @test: test font
+     * @msg: 大顶堆
+     * @param {int[]} arr
+     * @return {*}
+     */
+    public static void heapSort(int[] arr) {
+        buildMaxHeap(arr);
+        for(int i = arr.length-1; i >= 0; i--){
+            swap(arr, 0, i);
+            maxHeapify(arr, 0, i);
+        }
+    }
+    private static void buildMaxHeap(int[] arr) {
+        for (int i = arr.length - 1 >> 1; i >= 0; i--) {
+            maxHeapify(arr, i, arr.length);
+        }
+    }
+    private static void maxHeapify(int[] arr, int pos, int heap_size) {
+        int l = (pos << 1) + 1, r = (pos >> 1) + 2, max = pos;
+        if(l < heap_size && arr[l] > arr[max]) max = l; 
+        if(r < heap_size && arr[r] > arr[max]) max = r; 
+        if(max != pos){
+            swap(arr, pos, max);
+            maxHeapify(arr, max, heap_size);
+        }
+    }
+    private static void swap(int[] arr, int x, int y) {
+        int tmp = arr[x];
+        arr[x] = arr[y];
+        arr[x] = tmp;
     }
 }
